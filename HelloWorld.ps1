a65dfa96cb06ecb24f529ac623f20e14bd0a6ad5
Jeroen Rademaker <jcj.rademaker@gmail.com>
2020-09-08 11:42:54 +0200

    Hello world parameter testing

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -6,4 +6,6 @@ param(
 
 Write-Host "Hello World!"
 Write-Host "Parameter Test: $Test"y
+Write-Host "Parameter Arg0: $($args[0])" 
+Write-Host "Variables Demo: $Demo"
 Read-Host "Click Enter to continue..." 
