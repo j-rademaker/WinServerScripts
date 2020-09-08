32819f21b912644d56c6b6d9ce20be632fe8b963
Jeroen Rademaker <jcj.rademaker@gmail.com>
2020-09-08 18:01:44 +0200

    Hello world parameter testing extended

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -2,10 +2,12 @@
 
 param(
     $Test
+    $Test2
 )
 
 Write-Host "Hello World!"
-Write-Host "Parameter Test: $Test"y
-Write-Host "Parameter Arg0: $($args[0])" 
-Write-Host "Variables Demo: $Demo"
+Write-Host "Parameter Test:  $Test"
+Write-Host "Parameter Test2: $Test2"
+Write-Host "Parameter Arg0:  $($args[0])" 
+Write-Host "Variables Demo:  $Demo"
 Read-Host "Click Enter to continue..." 
